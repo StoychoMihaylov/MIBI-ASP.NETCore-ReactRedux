e0ef6e40d49c64fe0271268184e704e14e02c803
StoychoMihaylov <st.mihaylov.mihaylov@gmail.com>
2019-08-22 01:09:47 +0200

    sample request sending successful implemented

--- MIBI/Controllers/SampleController.cs
+++ MIBI/Controllers/SampleController.cs
@@ -1,9 +1,14 @@
 ﻿namespace MIBI.Controllers
 {
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
     using System.Linq;
     using System.Net;
     using MIBI.BindingModels;
     using MIBI.Services.Interfaces;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore.Http;
     using Microsoft.AspNetCore.Mvc;
 
     [Route("api/[controller]")]
@@ -11,10 +16,12 @@
     public class SampleController : Controller
     {
         private ISamleService service;
+        private IHostingEnvironment env;
 
-        public SampleController(ISamleService service)
+        public SampleController(ISamleService service, IHostingEnvironment env)
         {
             this.service = service;
+            this.env = env;
         }
 
         [HttpGet]
@@ -24,13 +31,45 @@
         }
 
         [HttpPost]
-        public IActionResult Post([FromBody]CreateNewSampleBindingModel bm)
+        public IActionResult Post(IFormCollection formData)
         {
-            var imgs = bm.Images;
-            foreach (var img in imgs)
-            {
+            
+            var name = Request.Headers["name"];
+            var description = Request.Headers["description"];
+            var tags = Request.Headers["tags"]
+                .ToString()
+                .Split(',')
+                .Select(tag => tag.TrimStart(' ').TrimEnd(' '))
+                .ToArray();
 
+            var newSample = new
+            {
+                Name = name,
+                Description = description,
+                Tags = tags,
+                Images = new List<string>()
+            };
+
+            try
+            {
+                foreach (var image in formData.Files)
+                {
+                    string path = Path.Combine(this.env.ContentRootPath + "\\Images");
+                    var newImgName = Guid.NewGuid().ToString();
+                    using (var img = new FileStream(Path.Combine(path, newImgName), FileMode.Create))
+                    {
+                        image.CopyToAsync(img);
+                        newSample.Images.Add(newImgName);
+                    }
+                }
             }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+
+
+            //TO DO: add new sample into the Database
 
             return Ok();
         }
