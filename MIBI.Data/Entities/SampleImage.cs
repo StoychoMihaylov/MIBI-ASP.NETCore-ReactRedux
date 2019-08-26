81cbfc3d9a595fbc2d5c85d585753e6adb9088aa
StoychoMihaylov <st.mihaylov.mihaylov@gmail.com>
2019-08-27 00:31:58 +0200

    create new sample service and functionality added

--- MIBI.Data/Entities/SampleImage.cs
+++ MIBI.Data/Entities/SampleImage.cs
@@ -1,20 +1,10 @@
 ﻿namespace MIBI.Data.Entities
 {
-    using System;
-
     public class SampleImage
     {
         public int Id { get; set; }
 
         public string Url { get; set; }
-
-        public string Name { get; set; }
-
-        public string Type { get; set; }
-
-        public DateTime CreatedOn { get; set; }
-
-        public string UploadedBy { get; set; }
 
         public virtual Sample Sample { get; set; }
     }
