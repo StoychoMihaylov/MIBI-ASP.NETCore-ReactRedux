2a251908414b585f13ffdbeda85ce25d47aeac3b
StoychoMihaylov <st.mihaylov.mihaylov@gmail.com>
2019-09-02 01:48:13 +0200

    name autocomplete added

---
+++ MIBI/ClientApp/src/actions/Sample/SampleActions.js
@@ -0,0 +1,86 @@
+﻿import {
+    REQUEST_ADD_NEW_SAMPLE,
+    REQUEST_ADD_NEW_SAMPLE_SUCCESS,
+    REQUEST_ADD_NEW_SAMPLE_FAIL,
+    REQUEST_GET_ALL_NAMES_OF_SAMPLES,
+    REQUEST_GET_ALL_NAMES_OF_SAMPLES_SUCCESS,
+    REQUEST_GET_ALL_NAMES_OF_SAMPLES_FAIL
+} from '../../constants/actionTypes'
+
+import { api } from '../../constants/api'
+import axios from 'axios'
+
+//********** Create New Sample **********
+
+export function addNewSampleInTheServer(imgFormdata) {
+    return function (dispatch) {
+        dispatch(createSample()) // Dispatch createNewSample
+        axios.post(api + 'api/sample', imgFormdata, {
+            headers: {
+                'Content-Type': 'multipart/form-data'
+            }
+        })
+        .then(result => {
+            dispatch(createSampleSuccess()) // Dispatch Successful created sample
+        })
+        .catch(err => {
+            console.log(err)
+            dispatch(createSampleFail(err)) // Dispatch for error
+        })
+    }
+}
+
+export function createSample() {
+    return {
+        type: REQUEST_ADD_NEW_SAMPLE
+    }
+}
+
+export function createSampleSuccess() {
+    return {
+        type: REQUEST_ADD_NEW_SAMPLE_SUCCESS,
+    }
+}
+
+export function createSampleFail(error) {
+    return {
+        type: REQUEST_ADD_NEW_SAMPLE_FAIL,
+        payload: error
+    }
+}
+
+//********** Sample actions **********
+
+export function getAllNamesOfExistingSamples() {
+    return function (dispatch) {
+        dispatch(getAllExistingNamesOfSamples()) // Dispatch get all ezisting names of samples
+        axios.get(api + 'api/autocomplete/names')
+        .then(response => {
+            dispatch(getAllExistingNamesOfSamplesSuccess(response["data"])) // Dispatch Successful request
+        })
+        .catch(err => {
+            console.log(err)
+            dispatch(getAllExistingNamesOfSamplesFail(err)) // Dispatch an error
+        })
+    }
+}
+
+export function getAllExistingNamesOfSamples() {
+    return {
+        type: REQUEST_GET_ALL_NAMES_OF_SAMPLES
+    }
+}
+
+export function getAllExistingNamesOfSamplesSuccess(data) {
+    return {
+        type: REQUEST_GET_ALL_NAMES_OF_SAMPLES_SUCCESS,
+        payload: data
+    }
+}
+
+export function getAllExistingNamesOfSamplesFail(error) {
+    return {
+        type: REQUEST_GET_ALL_NAMES_OF_SAMPLES_FAIL,
+        payload: error
+    }
+}
--- MIBI/ClientApp/src/view/Sample.js
+++ MIBI/ClientApp/src/view/Sample.js
@@ -1,31 +1,36 @@
 ﻿import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { Route } from 'react-router'
+import { getAllNamesOfExistingSamples } from '../actions/Sample/SampleActions'
 import "../styles/Sample.css"
 
 class SampleView extends Component {
-    constructor(props){
-        super(props)
 
-        this.state = {}
-    }
-
-    handleSearchBarClick(event) {
-        let text = event.target.value
-        alert(text)
+    async componentDidMount() {
+        await this.props.fetchAllNamesOfExistingSamples()
     }
 
     render() {
         return(
         <div className="conteiner">
             <div className="searchingBar">
-                <input
-                    placeholder="Search..."
-                    onChange={() => { }}
-                    onSearchClick={this.handleSearchBarClick}
-                    id="searchBar"
-                />
-                <button id="searchBtn"><img/></button>
+                <div>
+                    <input
+                        id="searchInput"
+                        placeholder="Search..."
+                        onChange={() => {}}
+                    />
+                    <div className="autocompleateNames">
+                        {
+                            this.props.autocompleteNamesOfSamples.map((rec, index) =>(
+                            <div key={index}>
+                                <p id={rec.id}>{rec.name}</p>
+                            </div>
+                        ))
+                        }
+                    </div>
+                </div>
+                <button id="searchBtn"><img alt=""/></button>
                 <Route render={({ history }) => (
                     <button
                         id="addSampleBtn"
@@ -41,4 +46,19 @@ class SampleView extends Component {
     }
 }
 
-export default connect()(SampleView);
+const mapStateToProps = state => {
+    console.log(state)
+    return {
+      autocompleteNamesOfSamples: state.sample.autocompleteNamesOfSamples,
+      isLoading: state.sample.isLoading,
+      error: state.sample.error
+    };
+  };
+
+const mapDispatchToProps = dispatch => {
+    return {
+        fetchAllNamesOfExistingSamples: () => dispatch(getAllNamesOfExistingSamples())
+    };
+  };
+
+export default connect(mapStateToProps, mapDispatchToProps)(SampleView)
