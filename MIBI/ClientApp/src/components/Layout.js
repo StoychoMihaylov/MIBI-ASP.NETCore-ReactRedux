614f520fa1fa42d471a2c5c28b7c054450e8dbbf
StoychoMihaylov <st.mihaylov.mihaylov@gmail.com>
2019-11-13 01:29:34 +0100

    notifications added

--- MIBI/ClientApp/src/components/Layout.js
+++ MIBI/ClientApp/src/components/Layout.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Container } from 'reactstrap';
 import NavMenu from './NavMenu';
+import Notification from '../components/Notification'
 
 export default props => (
   <div>
     <NavMenu />
+    <Notification/>
     <Container>
       {props.children}
     </Container>
