fe12d8864c2b2ed9add688f35c5a204228c07d09
StoychoMihaylov <st.mihaylov.mihaylov@gmail.com>
2019-11-16 02:37:42 +0100

    account controller and service unit tests added

---
+++ MIBI.UnitTests/Controllers/AccountControllerTest.cs
@@ -0,0 +1,46 @@
+﻿namespace MIBI.UnitTests.Controllers
+{
+    using Xunit;
+    using Microsoft.AspNetCore.Mvc;
+    
+    using MIBI.Controllers;
+    using MIBI.Models.BindingModels.Account;
+    using MIBI.Models.ViewModels.Account;
+    using MIBI.Services.Interfaces;
+    using System;
+    using Moq;
+
+    public class AccountControllerTest
+    {
+        [Fact]
+        public void Post_RegisterAndLogin_ShouldReturnStatusCode201()
+        {
+            // Arrange
+            var bidingModel = new RegisterUserBindingModel()
+            {
+                Name = "Gosho",
+                Email = "gosho@abv.bg",
+                Password = "P@ssl0rd",
+                ConfirmPassword = "P@ssl0rd"
+            };
+            var userCredentials = new AccountLoginViewModel()
+            {
+                UserId = new Guid(),
+                Token = "Token-Token-Token"
+            };
+
+            var serviceMock = new Mock<IAccountService>(); // Using Moq to mock the service
+            serviceMock
+                .Setup(s => s.CreateNewUserAccount(bidingModel))
+                .Returns(userCredentials);
+
+            var controller = new AccountController(serviceMock.Object);
+
+            // Act
+            var response = controller.RegisterAndLogin(bidingModel);
+
+            // Assert
+            Assert.IsType<OkObjectResult>(response);
+        }
+    }
+}
---
+++ MIBI.UnitTests/Services/AccountServiceTest.cs
@@ -0,0 +1,88 @@
+﻿namespace MIBI.UnitTests.Services
+{
+    using System;
+    using Xunit;
+    using System.Linq;
+
+    using MIBI.Data.Context;
+    using MIBI.Data.Entities;
+    using MIBI.Models.BindingModels.Account;
+    using MIBI.Models.ViewModels.Account;
+    using MIBI.Services.Services;
+    using Microsoft.EntityFrameworkCore;
+    
+
+    public class AccountServiceTest
+    {
+        [Fact]
+        public void CreateNewUserAccount_CalledWithCorrectInputData_ShouldReturnTrueAndUserCreated()
+        {
+            // Arrange
+            var createdUser = new User();
+            var db = this.GetDatabase();
+            var service = new AccountService(db);
+
+            RegisterUserBindingModel bm = new RegisterUserBindingModel()
+            {
+                Name = "Gosho",
+                Email = "gosho@abv.bg",
+                Password = "P@ssl0rd",
+                ConfirmPassword = "P@ssl0rd"
+            };
+
+            // Act
+            var tokenResponse = service.CreateNewUserAccount(bm);
+            if (tokenResponse != null)
+            {
+                // Check if user is created.
+                createdUser = db.Users.Single();
+            }
+
+            // Assert
+            Assert.NotNull(createdUser);
+        }
+
+        [Fact]
+        public void LoginUser_CalledWithRelevantInputData_ShouldReturnTokenBearer()
+        {
+            // Arrange
+            var userCredentials = new AccountLoginViewModel();
+            var db = this.GetDatabase();
+            var service = new AccountService(db);
+
+            RegisterUserBindingModel bm = new RegisterUserBindingModel()
+            {
+                Name = "Gosho",
+                Email = "gosho@abv.bg",
+                Password = "P@ssl0rd",
+                ConfirmPassword = "P@sslord"
+            };
+
+            LoginUserBindingModel loginForm = new LoginUserBindingModel()
+            {
+                Email = "gosho@abv.bg",
+                Password = "P@ssl0rd"
+            };
+
+            // Act
+            // Case with Correct Password Input
+            var isUserCreated = service.CreateNewUserAccount(bm);
+            if (isUserCreated != null)
+            {
+                userCredentials = service.LoginUser(loginForm);
+            }
+
+            // Assert
+            Assert.NotNull(userCredentials);
+        }
+
+        private MIBIContext GetDatabase()
+        {
+            var dbOptions = new DbContextOptionsBuilder<MIBIContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString()) // Microsoft.EntityFrameworkCore.InMemory
+                .Options;
+
+            return new MIBIContext(dbOptions);
+        }
+    }
+}
